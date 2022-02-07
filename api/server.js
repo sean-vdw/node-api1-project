9c3cdd629dc712ae01d9cd540691a7a036106de2
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-07 15:15:28 -0800

    completed all CRUD methods and error messages

--- api/server.js
+++ api/server.js
@@ -60,7 +60,28 @@ server.post('/api/users', async (req, res) => {
 });
 
 // --> Put/ Update User with Specified ID
-
+server.put('/api/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  const updatedUser = await Users.update(id, body);
+  try {
+    if (updatedUser === null || updatedUser === undefined) {
+      res.status(404).json({
+        message: 'The user with the specified ID does not exist'
+      })
+    } else if (!body.name || !body.bio) {
+      res.status(400).json({
+        message: 'Please provide name and bio for the user'
+      })
+    } else {
+      res.status(200).json(updatedUser);
+    }
+  } catch(err) {
+    res.status(500).json({
+      message: 'The user information could not be modified'
+    })
+  }
+});
 
 // --> Delete User
 server.delete('/api/users/:id', async (req, res) => {
