0b4823d4742bdbf9d3be7fab4272986f0817e581
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-07 13:40:23 -0800

    successfully fetching all users

--- api/server.js
+++ api/server.js
@@ -1,7 +1,28 @@
-// BUILD YOUR SERVER HERE
 const express = require('express');
+const Users = require('./users/model');
 
 const server = express();
 server.use(express.json());
 
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+// ENDPOINTS
+
+// --> Get All Users
+server.get('/api/users', (req, res) => {
+  Users.find()
+    .then(allUsers => {
+      res.json(allUsers);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `Could not fetch users...`,
+        error: err.message,
+      })
+    })
+});
+
+// --> Get User with Specific ID
+// --> Post New User
+// --> Put/ Update User with Specified ID
+// --> Delete User
+
+module.exports = server; 
