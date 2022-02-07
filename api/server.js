21b00751d0a31b000b301d7a1d4ace57e8fae012
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-07 13:52:25 -0800

    getting user by id

--- api/server.js
+++ api/server.js
@@ -21,6 +21,19 @@ server.get('/api/users', (req, res) => {
 });
 
 // --> Get User with Specific ID
+server.get('/api/users/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await Users.findById(id);
+    res.json(user);
+  } catch(err) {
+      res.status(500).json({
+        message: `Could not get user with id ${id}...`,
+        error: err.message,
+      })
+    }
+});
+
 // --> Post New User
 // --> Put/ Update User with Specified ID
 // --> Delete User
